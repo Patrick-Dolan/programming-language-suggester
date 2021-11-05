f1071176acd0f69149fb4f5003b4e7cd12bb10b3
Patrick Dolan <dolanp1992@gmail.com>
2021-11-05 14:01:58 -0700

    add functionality that submits answers to each question, hides the answered question and shows the next until it reaches the last question and shows all outcomes

--- js/scripts.js
+++ js/scripts.js
@@ -16,4 +16,44 @@ $(document).ready(function() {
     $("#suggestor-start-card").hide();
     $("#question-1").show();
   });
+  // Question 1 submit
+  $("#question-1").submit(function(event) {
+    event.preventDefault();
+    const question1Answer = $("input:radio[name=question-1-radios]:checked").val();
+    console.log(question1Answer);
+    hideQuestions();
+    $("#question-2").show();
+  });
+  // Question 2 submit
+  $("#question-2").submit(function(event) {
+    event.preventDefault();
+    const question2Answer = $("input:radio[name=question-2-radios]:checked").val();
+    console.log(question2Answer);
+    hideQuestions();
+    $("#question-3").show();
+  });
+  // Question 3 submit
+  $("#question-3").submit(function(event) {
+    event.preventDefault();
+    const question3Answer = $("input:radio[name=question-3-radios]:checked").val();
+    console.log(question3Answer);
+    hideQuestions();
+    $("#question-4").show();
+  });
+  // Question 4 submit
+  $("#question-4").submit(function(event) {
+    event.preventDefault();
+    const question4Answer = $("input:radio[name=question-4-radios]:checked").val();
+    console.log(question4Answer);
+    hideQuestions();
+    $("#question-5").show();
+  });
+  // Question 5 submit
+  $("#question-5").submit(function(event) {
+    event.preventDefault();
+    const question5Answer = $("input:radio[name=question-5-radios]:checked").val();
+    console.log(question5Answer);
+    hideQuestions();
+    $("#frontend-suggestion,#backend-suggestion,#app-development-suggestion").show();
+  });
 });
