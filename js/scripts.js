9f6b0747dbec2e78c74a0ec3ce7882f0b041e955
Patrick Dolan <dolanp1992@gmail.com>
2021-11-04 19:11:23 -0700

    add function to hide all question divs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+function hideQuestions() {
+  $("#question-1").hide();
+  $("#question-2").hide();
+  $("#question-3").hide();
+  $("#question-4").hide();
+  $("#question-5").hide();
+}
+
 $(document).ready(function() {
   
 });
